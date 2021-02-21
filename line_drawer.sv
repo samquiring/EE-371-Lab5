e2fd44a0edb65d080e24390dbafc0b270921f86b
Sam Quiring <lafcrules@gmail.com>
2021-02-21 11:33:46 -0800

    added starter code

---
+++ line_drawer.sv
@@ -0,0 +1,33 @@
+/* Given two points on the screen this module draws a line between
+ * those two points by coloring necessary pixels
+ *
+ * Inputs:
+ *   clk    - should be connected to a 50 MHz clock
+ *   reset  - resets the module and starts over the drawing process
+ *	 x0 	- x coordinate of the first end point
+ *   y0 	- y coordinate of the first end point
+ *   x1 	- x coordinate of the second end point
+ *   y1 	- y coordinate of the second end point
+ *
+ * Outputs:
+ *   x 		- x coordinate of the pixel to color
+ *   y 		- y coordinate of the pixel to color
+ *   done	- flag that line has finished drawing
+ *
+ */
+module line_drawer(clk, reset, x0, y0, x1, y1, x, y, done);
+	input logic clk, reset;
+	input logic [10:0]	x0, y0, x1, y1;
+	output logic done;
+	output logic [10:0]	x, y;
+	
+	/* You'll need to create some registers to keep track of things
+	 * such as error and direction.
+	 */
+	logic signed [11:0] error;  // example - feel free to change/delete
+	
+	always_ff @(posedge clk) begin
+		// YOUR CODE HERE
+	end  // always_ff
+	
+endmodule  // line_drawer
